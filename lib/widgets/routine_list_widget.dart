38e8302aa26d8c6dc9b5dfadf57819a806f62feb
gianlucaparadise <gianlucaparadise@gmail.com>
2020-01-09 07:43:47 +0100

    Selected Routine handling

--- lib/widgets/routine_list_widget.dart
+++ lib/widgets/routine_list_widget.dart
@@ -1,3 +1,6 @@
+import 'package:cast_your_instructions_flutter/cast/cast_manager.dart';
+import 'package:provider/provider.dart';
+
 import '../models/routine.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +10,34 @@ class RoutineListWidget extends StatelessWidget {
   final RoutineList routineList;
   final Function(Routine) onTap;
 
+  Widget _getListTileForCastManager(CastManager castManager, Routine routine) {
+    bool isSelected = castManager.routine?.id == routine.id;
+
+    Widget textTitle;
+    if (isSelected) {
+      textTitle = Text(
+        "${routine.title}",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      );
+    } else {
+      textTitle = Text("${routine.title}");
+    }
+
+    return ListTile(
+      title: textTitle,
+      onTap: () => onTap?.call(routine),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: routineList.routines.length,
         itemBuilder: (context, index) {
           Routine routine = routineList.routines[index];
-          return ListTile(
-            title: Text("${routine.title}"),
-            onTap: () => onTap?.call(routine),
+          return Consumer<CastManager>(
+            builder: (context, castManager, child) =>
+                _getListTileForCastManager(castManager, routine),
           );
         });
   }
